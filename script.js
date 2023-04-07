548bfe41ca6d07f64d285a63a15e2b6452b33616
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2023-04-08 00:06:38 +0800

    Figured out how to add in edit and delete span within <li> tag

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ var submitBtn = document.getElementById("submit")
 
 submitBtn.addEventListener("click", func);
 
+var counter = 0;
+
+
 function func(){
 
     let newElement = document.createElement("li");
-    let newSpanEdit = document.createElement("span");
-    let newSpanDelete = document.createElement("span")
-
-    newSpanEdit.innerHTML = "edit"
-    newSpanDelete.innerHTML = "delete"
 
     inputValue = document.getElementById("text_values") 
 
@@ -19,11 +17,21 @@ function func(){
     }
 
     else{
-        newElement.innerText = inputValue.value;
+
+        counter += 1;
+
         document.getElementById("lists").appendChild(newElement);
 
+        var proc = "<li>" + inputValue.value + "<span>delete</span><span>edit</span>" + "</li>";
+        document.getElementById("lists").children[counter].innerHTML = proc;
+
         document.getElementById("text_values").value = "";
+
+
     }
     
 
 }
+
+
+
