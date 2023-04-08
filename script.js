8fc646d7f56e89502ddb71b1292221d903870f14
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2023-04-08 16:05:05 +0800

    Make edit and button disappear unless upon hover.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var submitBtn = document.getElementById("submit")
 
 submitBtn.addEventListener("click", func);
 
-var counter = 0;
+var counter = -1;
 
 
 function func(){
@@ -22,10 +22,12 @@ function func(){
 
         document.getElementById("lists").appendChild(newElement);
 
-        var proc = "<li>" + inputValue.value + "<span>delete</span><span>edit</span>" + "</li>";
+        var proc = "<li id= lists-item-" +  counter.toString()  +">" + inputValue.value + "<span>delete</span><span>edit</span>" + "</li>";
         document.getElementById("lists").children[counter].innerHTML = proc;
 
         document.getElementById("text_values").value = "";
+
+        // document.getElementById("lists").removeChild()
 
 
     }
